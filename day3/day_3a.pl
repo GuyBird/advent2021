d39f2ad8921ab6908c41fd7bde0e44a5e171bca7
Guy Bird <06gbird@gmail.com>
2021-12-03 08:42:08 +0000

    Add day 3 part 1

---
+++ day3/day_3a.pl
@@ -0,0 +1,37 @@
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $filename = 'day3_input';
+
+open(FH, '<', $filename) or die $!;
+my @numbers = <FH>;
+close(FH);
+
+my $size = length($numbers[0]);
+my @zero_count = (0) x $size;
+my @one_count = (0) x $size;
+
+for my $number (@numbers) {
+    for my $index (0 .. $size - 2) {
+        my $digit = substr $number, $index, 1;
+        $zero_count[$index]++ if ($digit eq 0);
+        $one_count[$index]++ if ($digit eq 1);
+    }
+}
+
+my $gamma = "";
+my $epsilon = "";
+for my $index (0 .. $size - 2) {
+    if ( $zero_count[$index] >  $one_count[$index]) {
+        $gamma = $gamma . "0";
+        $epsilon = $epsilon . "1";
+    } else {
+        $gamma = $gamma . "1";
+        $epsilon = $epsilon . "0";
+    }
+} 
+
+my $answer = eval("0b$gamma") * eval("0b$epsilon");
+print $answer;
