e084d923365c03eeed83386638487fa8ffb57c79
Guy Bird <06gbird@gmail.com>
2021-12-07 00:08:52 +0000

    Add day 6 part 1 solution

---
+++ day6/day6_a.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+my $input_days = 256;
+
+my $filename = 'day6_input';
+open(FH, '<', $filename) or die $!;
+my @fishes = split ',',<FH>;
+close(FH);
+
+
+for my $day (1 .. $input_days) {
+    my $new_fish = 0;
+    for my $fish (@fishes) {
+        $fish--;
+        if ($fish == -1) {
+            $fish = 6;
+            $new_fish++;
+        }
+    }
+    foreach(1 .. $new_fish) {
+        push @fishes, 8;
+    }
+    #showState(\@fishes, $day);
+}
+
+print scalar @fishes;
+
+sub showState {
+    my ($fishes, $day) = @_;
+    print "After $day day: ";
+    for my $fish (@$fishes) {
+        print "$fish, ";
+    }
+    print "\n";
+}
