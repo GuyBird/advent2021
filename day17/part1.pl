e769d5bfd0cfd263a841f44bd0f00357ecf1c6bb
Guy Bird <06gbird@gmail.com>
2021-12-17 20:02:22 +0000

    Shorten dat 17 part 1 solution

--- day17/part1.pl
+++ day17/part1.pl
@@ -1,29 +1,12 @@
 use strict;
 use warnings;
 
-#x position can be ignored, horizontal and vertical velocities are decoupled
-my ($y_vel, $min_y);
-
+my ($y_vel, $min_y, $max_y);
 while (<>) {
-    if ($_ =~ /target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/) {
-        $min_y = $3;
-    } else {
-        print "[ERROR] input REGEX did not match\n";
-    }
-
+    $min_y = $3 if ($_ =~ /target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/);
 }
 
-#velocity decreases by 1 each time, ans starts at zero, 
-# therefore it will always pass theough 0 on the way down.
-#Thus the higest y velocity possible will pass through zero
-# on the step before hitting the target area, at the lower 
-# bound of the area.
-# The y velocity of the object at the final step will therefore
-# be equal to -(starting velocity + 1)
-
 $y_vel = abs($min_y) - 1;
-
-my $max_y = 0;
 
 while ($y_vel > 0) {
     $max_y += $y_vel;
